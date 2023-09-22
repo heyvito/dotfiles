6fb2e661865cd0c181fa1bdfe6e17e19bc1907c5
Victor Gama <hey@vito.io>
2023-09-22 18:46:53 -0300

    nvim: Update theme

--- nvim/.config/nvim/lua/vito/plugins/noirbuddy.lua
+++ nvim/.config/nvim/lua/vito/plugins/noirbuddy.lua
@@ -1,15 +1,29 @@
+-- return {
+--  'jesseleite/nvim-noirbuddy',
+--  as = 'noirbuddy',
+--  priority = 1000,
+--  lazy = false,
+--  dependencies = {
+--    {"tjdevries/colorbuddy.nvim", branch = "dev"},
+--  },
+--  config = function()
+--    local Color, colors, Group, groups, styles = require('colorbuddy').setup {}
+--    Group.new('NeoTreeFloatTitle', colors.primary)
+--    Group.new('NeoTreeTitleBar', colors.primary)
+--    require("noirbuddy").setup()
+--  end
+--}
+
 return {
-  'jesseleite/nvim-noirbuddy',
-  as = 'noirbuddy',
+  'aktersnurra/no-clown-fiesta.nvim',
+  as = 'no-clown-fiesta',
   priority = 1000,
   lazy = false,
-  dependencies = {
-    {"tjdevries/colorbuddy.nvim", branch = "dev"},
-  },
   config = function()
-    local Color, colors, Group, groups, styles = require('colorbuddy').setup {}
-    Group.new('NeoTreeFloatTitle', colors.primary)
-    Group.new('NeoTreeTitleBar', colors.primary)
-    require("noirbuddy").setup()
+    require('no-clown-fiesta').setup({
+      type = { bold = true },
+      lsp = { underline = true },
+    })
+    vim.cmd[[colorscheme no-clown-fiesta]]
   end
 }
